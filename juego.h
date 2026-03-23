0d59498baf1be2af9e747f015c2176d9572b9b80
KawRyy <clipsdekawry@gmail.com>
2026-03-23 16:06:43 +0100

    Update juego.h

--- juego.h
+++ juego.h
@@ -1,5 +1,10 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
 void buclePartida();
 static void Describir_Sala(int n);
 static void Inventario();
 static void Examinar();
 static void accionMover();
+
+#endif
