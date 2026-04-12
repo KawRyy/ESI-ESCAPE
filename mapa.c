53c9dcc23ded77154857573ef8a3028d516ab710
yunkera <carlosjunquera3@gmail.com>
2026-04-12 22:08:46 +0200

    mapa.c

--- mapa.c
+++ mapa.c
@@ -5,10 +5,15 @@
 #include "mapa.h"    
 #include "partida.h" 
 
+//Modulo que se encarga de gestionar las acciones relacionadas con el mapa, como examinar las salidas de la sala actual o moverse a otra sala, gestionando las salas y las conexiones
+
 void ExaminarSalidas(Partida *par);
 void accionMover(Partida *par, Salas *sal);
 
 void ExaminarSalidas(Partida *par){
+//Precondición: Deben haber sido cargados los datos de la partida y haber sido seleccionada la opción de examinar salidas en el menú de acciones del jugador
+//Postcondición: Se muestra la descripción de las conexiones que parten de la sala actual, indicando si están abiertas o bloqueadas.
+
     for(int i = 0; i < par->num_conexiones; i++){
         if(par->lista_conexiones[i].id_sala_orig == par->id_sala_actual){ // Si la conexión es desde la sala actual 
             if(par->lista_conexiones[i].estado_conexion == 1){ // Si la conexión está abierta
@@ -21,7 +26,9 @@ void ExaminarSalidas(Partida *par){
 }
 
 void accionMover(Partida *par, Salas *sal){ // Función para mover al jugador a otra sala siempre que esté abierta
- 
+ //Precondición: Deben haber sido cargados los datos de la partida y haber sido seleccionada la opción de moverse en el menú de acciones del jugador
+//Postcondición: Se muestra la descripción de las conexiones que parten de la sala actual y se pregunta al jugador si desea moverse a alguna de las salas conectadas.
+
    int j = 0; // Indica si se ha encontrado una conexión abierta desde la sala actual
     for(int i = 0; i < par->num_conexiones; i++){ // Recorre todas las conexiones para encontrar las que parten de la sala actual
         if(par->lista_conexiones[i].id_sala_orig == par->id_sala_actual && par->lista_conexiones[i].estado_conexion == 1){ // Si la conexión es desde la sala actual y está abierta
@@ -37,7 +44,7 @@ void accionMover(Partida *par, Salas *sal){ // Función para mover al jugador a 
             }
             
         }
-        if(j == 0){
+        if(j == 0){ // Si no se ha encontrado ninguna conexión abierta desde la sala actual
             printf("No hay conexiones abiertas desde esta sala.\n");
         }
     }
