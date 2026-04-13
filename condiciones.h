a242bf489f844848fbca973332d46642539411a5
yunkera <carlosjunquera3@gmail.com>
2026-04-13 22:45:20 +0200

    Copiar las estructuras correspondientes a condiciones.h condiciones.h

--- condiciones.h
+++ condiciones.h
@@ -3,6 +3,22 @@
 
 #include "estructuras.h"
 
+typedef struct {
+    char id_objeto[5];
+    char nombre_objeto[16];
+    char descripcion_objeto[51];
+    int localizacion_objeto;
+} Objetos;
+
+typedef struct {
+    char id_puzle[5];
+    int id_sala_puzle; // Sala donde se encuentra
+    int tipo_elemento; // Elemento que genera resolver el puzle, 1: CÓDIGO ; 2: PALABRA
+    char descripcion_puzle[151];
+    char solucion_puzle[51];
+    int resuelto; // 0: PENDIENTE ; 1: RESUELTO
+} Puzles;
+
 void ExaminarObjeto(Partida *par);  
 void Inventario(Partida *par, Jugadores *jug);
 void CogerObjeto(Jugadores *jug, Partida *par);
